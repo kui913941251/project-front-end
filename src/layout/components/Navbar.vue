9584a4802d469aa62653ab6cba72ff93166d1b91
luoxikui <luoxikui@emay.cn>
2021-10-29 10:00:53 +0800

    用户名显示空值判断

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,7 +8,7 @@
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img :src="avatar" class="user-avatar" />
-          <span class="user-name">{{ userInfo.username }}</span>
+          <span class="user-name">{{ userInfo.username && userInfo.username }}</span>
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
