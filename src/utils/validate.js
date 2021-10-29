0299183c3ddcd7546ab4f9ca897ea5e627058862
luoxikui <luoxikui@emay.cn>
2021-10-29 13:42:19 +0800

    添加密码格式验证

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,11 +1,13 @@
 /**
- * Created by PanJiaChen on 16/11/18.
- */
-
-/**
  * @param {string} path
  * @returns {Boolean}
  */
 export function isExternal(path) {
   return /^(https?:|mailto:|tel:)/.test(path)
+}
+
+
+export function passwordValidate(value) {
+  let reg = /^[A-Za-z0-9]{6,16}$/
+  return reg.test(value)
 }
