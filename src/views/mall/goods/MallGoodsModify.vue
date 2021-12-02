5d8f5ebccb0f1d8023608fca9160fd459a6661d1
luoxikui <luoxikui@emay.cn>
2021-12-02 17:14:44 +0800

    商品添加类别选项

--- src/views/mall/goods/MallGoodsModify.vue
+++ src/views/mall/goods/MallGoodsModify.vue
@@ -7,6 +7,21 @@
       <el-form-item label="商品描述" prop="goodsDesc">
         <el-input type="textarea" v-model="goodsForm.goodsDesc" class="input-width-500"></el-input>
       </el-form-item>
+      <el-form-item label="商品选项">
+        <div class="goods-option" v-for="(option, index) in goodsForm.options" :key="index">
+          <div class="option-info">
+            选项类别{{ index + 1 }}: <el-input class="input-width-200" v-model="option.optionName"></el-input>
+            <el-button @click="handleAddOptionItem(option)" style="margin-left: 10px">添加子选项</el-button>
+          </div>
+          <div class="option-item" v-for="(optionItem, index2) in option.children" :key="index2">
+            <el-form-item :label="`子选项${index2 + 1}`">
+              <el-input v-model="optionItem.optionName" class="input-width-200"></el-input>
+              <i class="el-icon-remove" @click="handleRemoveOptionItem(option, index2)"></i>
+            </el-form-item>
+          </div>
+        </div>
+        <el-button @click="handleAddOption">添加父选项</el-button>
+      </el-form-item>
 
       <el-form-item>
         <el-button @click="handleSubmit">确 定</el-button>
@@ -27,6 +42,7 @@ export default {
         id: null,
         goodsName: '',
         goodsDesc: '',
+        options: [],
       },
       goodsFormRules: {
         goodsName: [{ required: true, trigger: 'blur', message: '请输入商品名' }],
@@ -35,7 +51,6 @@ export default {
   },
   created() {
     let id = this.$route.params.id
-    console.log(id)
     if (id) {
       this.goodsForm.id = id
       this.getGoodsDetail()
@@ -49,8 +64,27 @@ export default {
       if (res.success) {
         this.goodsForm.goodsName = res.data.goodsName
         this.goodsForm.goodsDesc = res.data.goodsDesc
+        this.goodsForm.options = res.data.options
         this.defaultChecked = res.data.authIdList
       }
+    },
+    handleAddOption() {
+      let option = {
+        id: null,
+        optionName: '类别' + (this.goodsForm.options.length + 1),
+        children: [],
+      }
+      this.goodsForm.options.push(option)
+    },
+    handleAddOptionItem(option) {
+      let optionItem = {
+        id: null,
+        optionName: '新选项',
+      }
+      option.children.push(optionItem)
+    },
+    handleRemoveOptionItem(parent, index) {
+      parent.children.splice(index, 1)
     },
     async handleSubmit() {
       let validateRes = await this.$refs.goodsForm.validate().catch((err) => err)
@@ -79,4 +113,20 @@ export default {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '~@/styles/variables.scss';
+.mall-goods-modify {
+  .goods-option {
+    .option-info {
+      margin-bottom: 10px;
+    }
+    .option-item {
+      i {
+        cursor: pointer;
+        margin-left: 10px;
+        color: $color-danger;
+      }
+    }
+  }
+}
+</style>
